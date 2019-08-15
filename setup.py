70232f449e35abb756f22912be4fea78c9fb5f58
wau <cincitmcnally@gmail.com>
2019-08-15 15:07:26 -0400

    require tensorflow beta1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages
 
 
 setup(name='keras-rl2',
-      version='1.0.1',
+      version='1.0.3',
       description='Deep Reinforcement Learning for Tensorflow 2 Keras',
       author='Taylor McNally',
       author_email='taylor.mcnally@emory.edu',
       url='https://github.com/wau/keras-rl2',
       license='MIT',
-      install_requires=['tensorflow==2.0.0-beta0'],
+      install_requires=['tensorflow==2.0.0-beta1'],
       extras_require={
           'gym': ['gym'],
       },
