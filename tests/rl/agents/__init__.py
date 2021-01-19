ff4cf4d06c65ae476bf3d3cb038ba0dc34b85349
ikkamens <ikkamens@amazon.com>
2021-01-19 12:20:10 +0100

    Repaired imports in tests, added instructions for new contributors on how to run tests

--- tests/rl/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from .dqn import DQNAgent, NAFAgent, ContinuousDQNAgent
-from .ddpg import DDPGAgent
-from .cem import CEMAgent
-from .sarsa import SarsaAgent, SARSAAgent
